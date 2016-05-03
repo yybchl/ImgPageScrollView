ad07e5aee28c920a089453627e8dc24be6399984
杨友彬 <987311160@qq.com>
2016-05-03 17:55:11 +0800

    Initial Commit

---
+++ YoYoY_ImgScrollViewTests/YoYoY_ImgScrollViewTests.m
@@ -0,0 +1,39 @@
+//
+//  YoYoY_ImgScrollViewTests.m
+//  YoYoY_ImgScrollViewTests
+//
+//  Created by zx on 16/5/3.
+//  Copyright © 2016年 cn.yoyoy.mw. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface YoYoY_ImgScrollViewTests : XCTestCase
+
+@end
+
+@implementation YoYoY_ImgScrollViewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
